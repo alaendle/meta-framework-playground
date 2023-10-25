df3f77ee426645a2ed66b866a430bf1a85014f54
Andreas Ländle <alaendle@users.noreply.github.com>
2023-10-25 08:37:16 +0000

    SSR for astro - but seems to work strange.

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# Playground
+
+## In order to experiment with various SSR frameworks.
+
+### Some Notes
+
+- Run `lighthouse` 
+  ```
+  lighthouse --chrome-flags="--headless --no-sandbox --disable-gpu" http://localhost:4200/
+  ```
--- astro/src/components/header.components.ts
+++ astro/src/components/header.components.ts
@@ -1,13 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     standalone: true,
     template: `<div>
-      <h1>{{ title }}</h1>
-      <p>New Blog Post</p>
+      <h1>{{ data }}</h1>
     </div>`,
   })
   export class HeaderComponent {
-    @Input() title: string | undefined;
+    static clientProviders = [provideHttpClient()];
+    static renderProviders = [HeaderComponent.clientProviders];
+    http = inject(HttpClient);
+    data: string;
+
+  constructor() {
+    this.data = "Hallo?";
+    this.getData().then((data) => this.data = data.title);  // both calls seem to be needed!?
+  }
+
+  ngOnInit() {
+    this.getData().then((data) => this.data = data.title);  // both calls seem to be needed!?
+  }
+
+  async getData(): Promise<any> {
+    return await firstValueFrom(
+      //this.http.get('/api/me', {  // relative URLS should be supported with angular 17
+      this.http.get('https://jsonplaceholder.typicode.com/todos/1', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        responseType: 'json'
+      })
+    );
+  };
   }
